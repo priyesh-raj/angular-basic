17e04cf802477a0c988c645342c14f6f71186437
priyesh-raj <priyesh.raj9@gmail.com>
2020-04-10 02:18:23 +0530

    5 files modified, 5 added, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { warningAlertComponent } from './warningalert/warningalert.component';
-import { successAlertComponent } from './successalert/successalert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ],
-  declarations: [ AppComponent, HelloComponent, warningAlertComponent, successAlertComponent,],
+  declarations: [ AppComponent,  WarningAlertComponent, SuccessAlertComponent,],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/warningalert/warningalert.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core'
-@Component({
-  selector: 'app-warningalert',
-  templateUrl: './warningalert/warningalert.component.html',
-  styleUrls: ['./warningalert/warningalert.component.css']
-})
-export class warningAlertComponent{}
