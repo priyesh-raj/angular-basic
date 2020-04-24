464b41d5682d5b75010515d3b0eb46dba1b44fa5
priyesh-raj <priyesh.raj9@gmail.com>
2020-04-25 04:48:27 +0530

    1 file modified

--- src/app/recepies/recepie-list/recepie-list.component.ts
+++ src/app/recepies/recepie-list/recepie-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recepie } from '../recepie.model'
-import { imageUrl } from '../../shared/image';
+// import { imageUrl } from '../../shared/image';
 @Component({
   selector: 'app-recepie-list',
   templateUrl: './recepie-list.component.html',
@@ -10,8 +10,8 @@ import { imageUrl } from '../../shared/image';
 export class RecepieListComponent implements OnInit {
   @Output() selectedElementDetails = new EventEmitter<Recepie>()
   recepies: Recepie[]= [
-    new Recepie('Test Recepie',imageUrl,'Test Recepie Description'),
-    new Recepie('Test Recepie 1',imageUrl ,'Test Recepie Description 1')
+    new Recepie('Test Recepie','https://cdn.pixabay.com/photo/2018/01/13/22/41/food-3080911_960_720.jpg','Test Recepie Description'),
+    new Recepie('Test Recepie 1','https://cdn.pixabay.com/photo/2018/01/13/22/41/food-3080911_960_720.jpg' ,'Test Recepie Description 1')
   ]
   constructor() { }
 
