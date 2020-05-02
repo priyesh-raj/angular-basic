971627abd85dda2491b2bf899e99fc661ad51b6c
priyesh-raj <priyesh.raj9@gmail.com>
2020-05-02 18:16:49 +0530

    1 file modified

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,13 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-
+  @HostBinding('class.open')isOpen = false
   constructor(private eleRef: ElementRef,) { }
-
+  
+  @HostListener('click',['$event']) onDropdownOpen(evtData: Event){
+    this.isOpen=this.eleRef.nativeElement.contains(evtData.target)? !this.isOpen: false
+  } 
 }
