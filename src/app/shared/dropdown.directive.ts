e074f39de4ac97cf94d39769c8e040bd2e458519
priyesh-raj <priyesh.raj9@gmail.com>
2020-05-02 18:25:25 +0530

    3 files modified

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -7,7 +7,7 @@ export class DropdownDirective {
   @HostBinding('class.open')isOpen = false
   constructor(private eleRef: ElementRef,) { }
   
-  @HostListener('click',['$event']) onDropdownOpen(evtData: Event){
+  @HostListener('documnet:click',['$event']) onDropdownOpen(evtData: Event){
     this.isOpen=this.eleRef.nativeElement.contains(evtData.target)? !this.isOpen: false
   } 
 }
