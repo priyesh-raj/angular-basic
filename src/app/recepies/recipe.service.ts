8adade633e4f7da74d9aa3f9fb9ffc57b87f0352
priyesh-raj <priyesh.raj9@gmail.com>
2020-05-03 21:04:59 +0530

    1 file modified

--- src/app/recepies/recipe.service.ts
+++ src/app/recepies/recipe.service.ts
@@ -9,7 +9,7 @@ export class RecipeService {
 
   recipes: Recepie[]= [
     new Recepie('Test Recepie','https://cdn.pixabay.com/photo/2018/01/13/22/41/food-3080911_960_720.jpg','Test Recepie Description',[new Ingredients('Corn',5),new Ingredients('Tortia',1)]),
-    new Recepie('Test Recepie 1','https://cdn.pixabay.com/photo/2018/01/13/22/41/food-3080911_960_720.jpg' ,'Test Recepie Description 1')
+    new Recepie('Test Recepie 1','https://cdn.pixabay.com/photo/2018/01/13/22/41/food-3080911_960_720.jpg' ,'Test Recepie Description 1',[new Ingredients('Buns',5),new Ingredients('Lettuce',1)])
   ]
   constructor() { }
   
